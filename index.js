3a50327422b06bd5762310bd6abd52bf9c49f74d
Rajesh D <dhrajesh1@gmail.com>
2020-04-30 11:39:21 +0530

    PORT address
    PORT address, dynamic done in console

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', function (req, res, next) {
 })
 
 app.listen(PORT, function () {
-    console.log('CORS-enabled web server listening on port 80')
+    console.log(`CORS-enabled web server listening on port ${PORT}`)
 })
