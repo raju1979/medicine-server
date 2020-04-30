051b11e87a5df20cacfb9756a9239ea66ac31ec4
Rajesh D <dhrajesh1@gmail.com>
2020-04-30 11:33:53 +0530

    PORT added for heroku

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const app = express()
+const app = express();
 
 require('dotenv').config();
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
 
-app.listen(5000, function () {
+app.listen(PORT, function () {
     console.log('CORS-enabled web server listening on port 80')
 })
