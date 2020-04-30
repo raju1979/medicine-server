e97484aa5d24c91732d23eee6827d68ac6749410
Rajesh D <dhrajesh1@gmail.com>
2020-04-30 16:09:23 +0530

    mongo initial integration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 
 require('dotenv').config();
@@ -8,6 +10,13 @@ require('dotenv').config();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb+srv://raju:2180450a@cluster0-il4kd.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => console.log('db connected'))
 
 app.get('/', function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
